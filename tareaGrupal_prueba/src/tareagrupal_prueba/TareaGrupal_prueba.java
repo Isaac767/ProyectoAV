d6761a08e84d1fb316480a8560fb776307359996
ANITA-16; <anacristinaguamangonzalez@gmail.com;>
2024-05-07 08:20:30 -0500

    subi un hola mundo de prueba

---
+++ tareaGrupal_prueba/src/tareagrupal_prueba/TareaGrupal_prueba.java
@@ -0,0 +1,12 @@
+
+public class TareaGrupal_prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HOLA MUNDO :3");
+    }
+    
+}
