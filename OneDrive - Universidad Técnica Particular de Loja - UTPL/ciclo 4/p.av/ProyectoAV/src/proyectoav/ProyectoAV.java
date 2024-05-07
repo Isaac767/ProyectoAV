ea4e1acdf5794b792dad1ef408db9df7d2147334
Isaac767 <stevenneira2019@gmail.com>
2024-05-07 08:01:22 -0500

    Primer cambio

---
+++ OneDrive - Universidad Técnica Particular de Loja - UTPL/ciclo 4/p.av/ProyectoAV/src/proyectoav/ProyectoAV.java
@@ -0,0 +1,12 @@
+package proyectoav;
+
+public class ProyectoAV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Mi Nombre");
+    }
+    
+}
